e6c2f5a3f1e95c87eb8fd20b8a6938e883190fdf
Tom Bowers <tom@invisual.co.uk>
2015-03-23 10:30:38 +0000

    Initial tweet cards work.

--- TwitterCards/App_Start/BundleConfig.cs
+++ TwitterCards/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Optimization;
-using TwitterCards.App_Start;
 
 namespace TwitterCards
 {
@@ -7,7 +6,20 @@ namespace TwitterCards
 	{
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new LessBundle("~/Bundles/Main").Include("~/Presentation/Css/Main.less"));
+			bundles.UseCdn = true;
+			BundleTable.EnableOptimizations = true; // force optimizations while debugging (including loading from cdn)
+
+			bundles.Add(new LessBundle("~/Bundles/MainCss").Include("~/Presentation/Css/Main.less"));
+			bundles.Add(new LessBundle("~/Bundles/DemoCss").Include("~/Presentation/Css/Demo.less"));
+
+			// jQuery from CDN with local backup
+			var jQueryBundle = new ScriptBundle("~/Bundles/jQuery", "//code.jquery.com/jquery-2.1.3.min.js").Include("~/Presentation/Javascript/jQuery/jquery-2.1.3.min.js");
+			jQueryBundle.CdnFallbackExpression = "window.jQuery";
+			bundles.Add(jQueryBundle);
+
+			bundles.Add(new ScriptBundle("~/Bundles/jQueryTransit").Include("~/Presentation/Javascript/jQuery/jquery.transit.js"));
+
+			bundles.Add(new ScriptBundle("~/Bundles/DemoJavascript").Include("~/Presentation/Javascript/Demo.js"));
 		}
 	}
 }
---
+++ TwitterCards/Presentation/Javascript/Demo.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+	var tweetContainer = $('.tweet-container');
+
+	$.getJSON('../api/data', function(data) {
+		$.each(data, function (key, val) {
+
+			var tweetCard = $('<div class="tweet-card"><div class="tweet-author">@' + val.Author + '</div><div class="tweet-text">'+ val.Text +'</div></div>');
+			tweetContainer.append(tweetCard);
+		});
+	});
+
+});
